6f2040b019c0677b54b0a1db8ac1bb238ed50d2a
zoeminondo <79917114+zoeminondo@users.noreply.github.com>
2021-12-01 14:04:35 +0100

    Update codeR.R
    Troisième ébauche : cours du 01/12

--- codeR.R
+++ codeR.R
@@ -3,4 +3,29 @@
 # DEVOIR MAISON 
 #
 #===============================================================================
-station <- read.table('station.txt', header=TRUE)
+station <- read.table("H:/personnel/Modélisation statistique/regressionLineaire/station.txt", header=TRUE)
+attach(station)
+
+plot(station)
+library(PCAmixdata)
+res <- PCAmix(station, graph=FALSE)
+round(res$eig, digit=2)
+head(station)
+
+
+
+# y variable à expliquer : les ventes des stations services
+res <- lm(ventes~., data=station)
+summary(res)
+summary(ventes)
+
+step(res)
+
+res1 <- lm(ventes~trafic+nbpompes)
+shapiro.test(res1$residuals)
+
+plot(res1$fitted, res1$residuals)
+abline(h=0)
+
+outlier<-which(abs(res1$residuals)<-10)
+outlier
